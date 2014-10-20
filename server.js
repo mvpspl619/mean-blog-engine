719a795180f320c2eb5d5d3e8d21cecbcd9b556c
unknown <mvpspl619@gmail.com>
2014-10-20 23:48:22 +0530

    Fixing server.js

--- server.js
+++ server.js
@@ -5,23 +5,15 @@ var app = express()
 var port = Number(process.env.PORT || 3000)
 var staticDirectory = "\\src"
 
-//app.use(function (req, res, next) {
-    //res.set('Access-Control-Allow-Origin', '*')
-    //res.set('Access-Control-Allow-Methods', 'GET')
-    //res.set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Accept')
-    //next()
-//});
-
-app.use('/src', express.static(__dirname + staticDirectory))
-
-app.use('/api/posts', postsRetriever.getAllPosts)
-
-app.use('/api/post/:id', postsRetriever.getSinglePost)
-
-app.get('/*', function(req, res){
-	res.status(200).sendFile(__dirname + '/index.html')
+app.use('/src', express.static(__dirname + '/src'))
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
 })
-
+app.get('/api/posts', postsRetriever.getAllPosts)
+app.get('/api/post/:id', postsRetriever.getSinglePost)
+app.use(function(req, res){
+	res.sendFile(__dirname + '/index.html');
+})
 module.exports = app.listen(port, function(){
 	console.log("Express server listening on port " + port)
 })
